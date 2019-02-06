16fa494daa7691dfe75d88533cf221098a8d7098
dutdavid <dutdavid7@gmail.com>
2019-02-06 10:43:17 +0300

    chsnge

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,11 +1,99 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import org.sql2o.*;
+
 public class Task {
-    private String mDescription;
+  private String description;
+  private boolean completed;
+  private LocalDateTime createdAt;
+  private int id;
+
+  public static List<Task> all() { //returns all instances of  the task class
+    String sql = "SELECT id, description FROM tasks"; //requests all id and description from the tasks table
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).executeAndFetch(Task.class);
+    }
+  }
+/*
+  public void save() { //creating the save method
+    try(Connection con = DB.sql2o.open()) { //establishing a connection
+      String sql = "INSERT INTO tasks (description) VALUES (:description)"; //we construct an sql statement that uses the placeholder description--protects from sql injection
+      con.createQuery(sql) //calling sql query passing in the sql statements
+        .addParameter("description", this.description) //we replace the :description placeholder with this.description using .add
+        .executeUpdate(); //we run the query
+    }
+  }  */
+
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO tasks(description) VALUES (:description)";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("description", this.description)
+        .executeUpdate()
+        .getKey();
+    }
+  }
+
+  @Override
+  public boolean equals(Object otherTask){
+    if (!(otherTask instanceof Task)) {
+      return false;
+    } else {
+      Task newTask = (Task) otherTask;
+      return this.getDescription().equals(newTask.getDescription()) &&
+             this.getId() == newTask.getId();
+    }
+  }
   
+  public Task(String description) {
+    this.description = description; //refers to the description property. required when a property name is exact same as a local variable
+    //that is , because our argument is named description, we need to specify this.desc referring to task object's desc
+    completed = false;
+    createdAt = LocalDateTime.now();
+  }
+  
+  public String getDescription() {
+    return description;
+  }
+
+  public boolean isCompleted() {
+    return completed;
+  }
+
+  public LocalDateTime getCreatedAt() {
+    return createdAt;
+  }
+
+  public int getId() {
+    return id;
+  }
+
+  public static Task find(int id) {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "SELECT * FROM tasks where id=:id";
+      Task task = con.createQuery(sql)
+        .addParameter("id", id)
+        .executeAndFetchFirst(Task.class);
+      return task;
+    }
+  }
+}
+
+
+
+  /*
+    private String mDescription; //m is removed above coz all properties must match
+    the names of the corresponding database columns
+  and preceding database column names with an m is not the standard naming convention
     public Task(String description) {
       mDescription = description;
     }
+
+    //we remove instances arraylist because our database will not be responsible for keeping track of tasks
   
     public String getDescription() {
       return mDescription;
     }
   }
+  */
--- src/test/java/TaskTest.java
+++ src/test/java/TaskTest.java
@@ -1,8 +1,75 @@
+import org.sql2o.*;
 import org.junit.*;
 import static org.junit.Assert.*;
-
 public class TaskTest {
     
+  @Before //instructs JUNIt to run this code before each test
+  public void setUp() { // the setup method creates a new instance of sql2o
+    DB.sql2o = new Sql2o("jdbc:postgresql://localhost:5432/to_do_test", "postgres", "dut");
+  }
+
+  @After  //deletes all our sample task objects after running each test since leftover objects can cause unexpected test failures
+  public void tearDown() {
+    try(Connection con = DB.sql2o.open()) { //try without resources-usefull when creating a two-way data stream thus closes the connection after the code is executed
+      //the sql2o connection object is done by calling open() on the DV.sql2o
+      String sql = "DELETE FROM tasks *;"; // deletes everything from task table
+      con.createQuery(sql).executeUpdate(); //con. creates a connection by calling createquery on con and pass in the sql
+   // the execute update automatically executes sql commands 
+    }
+
+  }
+
+  @Test
+  public void equals_returnsTrueIfDescriptionsAretheSame() {
+    Task firstTask = new Task("Mow the lawn");
+    Task secondTask = new Task("Mow the lawn");
+    assertTrue(firstTask.equals(secondTask));
+  }
+
+
+
+@Test
+public void save_returnsTrueIfDescriptionsAretheSame() {
+  Task myTask = new Task("Mow the lawn");
+  myTask.save(); //responsible for inserting this object into our database
+  assertTrue(Task.all().get(0).equals(myTask)); //all retrieves Task objects
+//.equals confirms the Task returned from our database is same as the Task we created localy
+}
+
+@Test
+  public void all_returnsAllInstancesOfTask_true() {
+    Task firstTask = new Task("Mow the lawn");
+    firstTask.save();
+    Task secondTask = new Task("Buy groceries");
+    secondTask.save();
+    assertEquals(true, Task.all().get(0).equals(firstTask));
+    assertEquals(true, Task.all().get(1).equals(secondTask));
+  }
+
+@Test
+public void save_assignsIdToObject() {
+  Task myTask = new Task("Mow the lawn");
+  myTask.save();
+  Task savedTask = Task.all().get(0);
+  assertEquals(myTask.getId(), savedTask.getId());
+}
+
+@Test
+  public void getId_tasksInstantiateWithAnID() {
+    Task myTask = new Task("Mow the lawn");
+    myTask.save();
+    assertTrue(myTask.getId() > 0);
+  }
+
+  @Test
+  public void find_returnsTaskWithSameId_secondTask() {
+    Task firstTask = new Task("Mow the lawn");
+    firstTask.save();
+    Task secondTask = new Task("Buy groceries");
+    secondTask.save();
+    assertEquals(Task.find(secondTask.getId()), secondTask);
+  }
+
     @Test
     public void Task_instantiatesWithDescription_String() {
       Task myTask = new Task("Mow the lawn");
